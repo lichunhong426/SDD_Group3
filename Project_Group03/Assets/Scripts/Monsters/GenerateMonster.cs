d2f25afe9dd7b3ed372a19dd3e3fa4537cbcb228
lichunhong426 <210108464@stu.vtc.edu.hk>
2022-05-21 18:12:41 +0800

    Update GenerateMonster.cs

--- Project_Group03/Assets/Scripts/Monsters/GenerateMonster.cs
+++ Project_Group03/Assets/Scripts/Monsters/GenerateMonster.cs
@@ -11,7 +11,15 @@ public class GenerateMonster : MonoBehaviour
     float nextSpawn = 0.0f;
     float spawnRate = 2f;
 
-
+    private void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            xPos = Random.Range(10, 120);
+            zPos = Random.Range(60, 190);
+            foreach (GameObject monster in monsters)
+            {
                 Instantiate(monster, new Vector3(xPos, 0, zPos), Quaternion.identity);
             }
         }
